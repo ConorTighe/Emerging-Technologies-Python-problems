6dc6e98bc271fb3ee0ffbdc5948ce2980ef5b221
ConorTighe1995 <conor.tighe22@gmail.com>
2017-09-20 12:55:26 +0100

    Problem 6

--- Prob6.py
+++ Prob6.py
@@ -1,1 +1,21 @@
 # Smallest and largest in list
+import random
+
+numbers = [2,4,6,1,3,7,9,10,5,8]
+smallest = 1
+largest = 1
+cnt = 0
+
+while cnt <= 10:
+    i = random.randint(1, 20)
+    cnt+=1
+    numbers.append(i)
+
+for x in numbers:
+    print(x)
+    if smallest < x:
+        smallest = x
+    if largest > x:
+        largest = x
+
+print("The largest numer in the list is %s and the smallest is %s", largest, smallest)
