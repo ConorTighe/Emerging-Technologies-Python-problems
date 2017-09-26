abc1bd105fec8ce922bea709e0775033c728cd8a
ConorTighe1995 <conor.tighe22@gmail.com>
2017-09-26 16:46:30 +0100

    Problem 8 fixed

--- Prob8.py
+++ Prob8.py
@@ -1,35 +1,10 @@
 
-myList1 = [1,3,5,7,9]
-myList2 = [2,4,6,8]
+myList1 = [50,3,5,7,9]
+myList2 = [51,4,6,8]
 fullList = []
-i=0
-j=0
-cnt=0
-List1Len = len(myList1)
-List2Len = len(myList2)
-size = (List1Len + List2Len)-1
-print(myList1)
-print(myList2)
-print("total numbers in both lists:", size+1)
 
-while cnt < size:
-    if myList1[i] < myList2[j]:
-        fullList.append(myList1[i])
-        i = i + 1
-
-    elif myList1[i] > myList2[j]:
-        fullList.append(myList2[j])
-        j = j + 1
-
-    cnt+=1
-
-List1Len-=1
-List2Len-=1
-
-if myList1[List1Len] < myList2[List2Len]:
-    fullList.append(myList2[List2Len])
-elif myList1[List1Len] > myList2[List2Len]:
-    fullList.append(myList1[List1Len])
+fullList = myList1 + myList2
+fullList.sort()
 
 print("Combined ordered list: ")
 for l in fullList:
